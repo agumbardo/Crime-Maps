5d1299269123477068511abff0095db38dd849e5
Vedaant Paul Kukadia <vpk@mit.edu>
2019-12-11 02:33:18 -0500

    added plots to website, and data

--- Website/website.py
+++ Website/website.py
@@ -14,6 +14,15 @@ def chicago():
 def about():
 	return render_template("about.html")
 
+@app.route("/vis1")
+def vis1():
+	return render_template("vis1.html")
+
+@app.route("/vis2")
+def vis2():
+	return render_template("vis2.html")
+
+
 
 if __name__ == "__main__":
 	app.run()
